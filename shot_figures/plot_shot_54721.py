68fd5828cd332d128fa47d264eb19bb619d576e1
Julien Hillairet <julien.hillairet@gmail.com>
2019-08-07 15:50:00 +0200

    added figures short 54719, 21 and 32
    some far the best of the week

--- shot_figures/plot_shot_54721.py
+++ shot_figures/plot_shot_54721.py
@@ -14,11 +14,11 @@ from matplotlib.pyplot import *
 
 
 #%%
-pulse = 54732
+pulse = 54721
 
 P_IC_tot, t_tot = get_sig(pulse, signals['IC_P_tot'])
-#P_Q1, t_Q1 = get_sig(pulse, signals['IC_P_Q1'])
-#P_Q2, t_Q2 = get_sig(pulse, signals['IC_P_Q2'])
+P_Q1, t_Q1 = get_sig(pulse, signals['IC_P_Q1'])
+P_Q2, t_Q2 = get_sig(pulse, signals['IC_P_Q2'])
 P_Q4, t_Q4 = get_sig(pulse, signals['IC_P_Q4'])
 
 P_LH_tot, t_LH_tot = get_sig(pulse, signals['LH_P_tot'])
@@ -35,6 +35,7 @@ fig, ax = plt.subplots()
 ax.fill_between(t_tot, _P_LH_tot*1e3 + P_IC_tot.squeeze(), alpha=0.2, label='Total RF Power')
 ax.plot(t_LH1, P_LH1, label='LH1', lw=2)
 ax.plot(t_LH2, P_LH2, label='LH2', lw=2)
+#ax.plot(t_Q1, P_Q1, label='IC Q1', lw=2)
 #ax.plot(t_Q2, P_Q2, label='IC Q2', lw=2)
 ax.plot(t_Q4, P_Q4, label='IC Q4', lw=2)
 
@@ -48,5 +49,5 @@ ax.set_xlabel('Time [s]', fontsize=14)
 
 fig.tight_layout()
 #%%
-savefig('WEST_IC_54732.png', dpi=150)
+savefig('WEST_IC_54721.png', dpi=150)
 
